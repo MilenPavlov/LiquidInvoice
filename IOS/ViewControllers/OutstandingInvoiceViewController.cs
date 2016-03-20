ef4978ea9639dac5e518fffec949a8810516140a
Jason Feinkind <JFeinkind@hudsonalley.com>
2016-03-19 20:06:24 -0400

    refactor directory structure

---
+++ IOS/ViewControllers/OutstandingInvoiceViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace MobileIOS
+{
+    public partial class OutstandingInvoiceViewController : UIViewController
+    {
+        public OutstandingInvoiceViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
