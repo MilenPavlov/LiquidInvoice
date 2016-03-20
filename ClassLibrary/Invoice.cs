5b182ebc8f463e3833ab3a77f6c1fd31e16df5cd
Jason Feinkind <JFeinkind@hudsonalley.com>
2016-03-19 21:04:57 -0400

    added new models and tables in db

---
+++ ClassLibrary/Invoice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ClassLibrary
+{
+	public class Invoice : Resource
+	{
+		public int CompanyId { get; set; }
+
+		public int CustomerId { get; set; }
+
+		public decimal AmountDue { get; set; }
+
+		public DateTime DueDate { get; set; }
+	}
+}
+
