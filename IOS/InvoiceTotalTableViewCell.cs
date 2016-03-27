c247f80bcda5add491d9723f2cadaf25dc791819
Jason Feinkind <JFeinkind@hudsonalley.com>
2016-03-27 00:30:56 -0400

    Finish UI work for invoice page

---
+++ IOS/InvoiceTotalTableViewCell.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace MobileIOS
+{
+    public partial class InvoiceTotalTableViewCell : UITableViewCell
+    {
+        public InvoiceTotalTableViewCell (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
